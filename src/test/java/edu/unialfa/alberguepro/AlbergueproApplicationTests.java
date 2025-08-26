201883d567c3a778e7c426016e24666239051000
Danilo <danilorosa82@hotmail.com>
2025-08-26 20:31:16 -0300

    Adicionando projeto inicial do TCC

---
+++ src/test/java/edu/unialfa/alberguepro/AlbergueproApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unialfa.alberguepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbergueproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
