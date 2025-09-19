9bf897ac1ebbbe86412a7eff319f2acc1de5a965
gabriel7419 <sgtgabriel7419@gmail.com>
2025-09-19 19:31:38 -0300

    fix: Corrigindo erros de compilação

--- src/main/java/edu/unialfa/alberguepro/config/DataInitializer.java
+++ src/main/java/edu/unialfa/alberguepro/config/DataInitializer.java
@@ -1,14 +1,21 @@
 package edu.unialfa.alberguepro.config;
 
+import edu.unialfa.alberguepro.model.Unidade;
 import edu.unialfa.alberguepro.model.Usuario;
+import edu.unialfa.alberguepro.repository.UnidadeRepository;
 import edu.unialfa.alberguepro.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
 @Component
 public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private UnidadeRepository unidadeRepository;
 
     @Autowired
     private UsuarioRepository usuarioRepository;
@@ -34,6 +41,20 @@ public class DataInitializer implements CommandLineRunner {
             System.out.println("Usuário administrador criado com sucesso!");
         } else {
             System.out.println("Usuário administrador já existe. Nenhuma ação necessária.");
+        }
+
+        // Verifica se as unidades de medida já existem
+        if (unidadeRepository.count() == 0) {
+            System.out.println("Criando unidades de medida padrão...");
+
+            Unidade un1 = new Unidade(); un1.setNome("Unidade");
+            Unidade un2 = new Unidade(); un2.setNome("Caixa");
+            Unidade un3 = new Unidade(); un3.setNome("Pacote");
+            Unidade un4 = new Unidade(); un4.setNome("Litro");
+            Unidade un5 = new Unidade(); un5.setNome("Kg");
+
+            unidadeRepository.saveAll(Arrays.asList(un1, un2, un3, un4, un5));
+            System.out.println("Unidades de medida criadas com sucesso!");
         }
     }
 }
